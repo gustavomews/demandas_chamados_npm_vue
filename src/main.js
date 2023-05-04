61710dcb5e60aee871db1f1f33a72c1e42c10773
Gustavo Mews <gustavofariasmews@gmail.com>
2023-05-03 22:10:11 -0300

    C7 - Melhorias na separação de Assets

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.bundle';
-import './assets/style.css'
+import './assets/css/style.css'
 
 // INTERCEPTORS FROM AXIOS
 import axios from 'axios'
