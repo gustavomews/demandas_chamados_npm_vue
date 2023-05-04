8ed71cf7e3decc0dcb0bc43fc34e304e14bcc3a8
Gustavo Mews <gustavofariasmews@gmail.com>
2023-05-03 23:05:14 -0300

    C9 - Criação do componente Card e montagem inicial da tela de Início

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div :class="'col-' + col">
+            <div class="card mb-3">
+                <div class="card-header" v-if="title">
+                    <i v-if="icon" :class="icon"></i>
+                    {{ title }}
+                </div>
+
+                <div class="card-body">
+                    <slot name="body"></slot>
+                </div>
+
+                <div class="card-footer" v-if="$slots.footer">
+                    <slot name="footer"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: ['col', 'title', 'icon']
+}
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
     <Navbar />
+    <Card col="12" :title="'Seja bem vindo(a) ' + $store.state.name" icon="fa-regular fa-star">
+      <h4 slot="body">Dashboard</h4>
+    </Card>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import Card from '../components/Card.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    Navbar
+    Navbar, Card
   }
 }
 </script>
