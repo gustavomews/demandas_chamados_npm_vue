6396e460a06b2986da4bb152e528177373f59197
Gustavo Mews <gustavofariasmews@gmail.com>
2023-05-03 23:33:05 -0300

    C11 - Início da view de demandas

--- src/views/DemandsView.vue
+++ src/views/DemandsView.vue
@@ -1,14 +1,50 @@
 <template>
-  <Navbar />
+  <div class="demands">
+    <Navbar />
+    <Card col="12" title="Demandas" icon="fa-solid fa-list-ul">
+      <div slot="body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Card>
+  </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import Card from '../components/Card.vue'
 
 export default {
   name: 'DemandsView',
   components: {
-    Navbar
+    Navbar, Card
   }
 }
 </script>
